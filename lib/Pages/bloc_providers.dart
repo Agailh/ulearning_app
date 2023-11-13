877df1d43da3d769b1a3a5dc00500d468bc7016b
Aga Ilham <agailham86@gmail.com>
2023-11-13 11:26:05 +0700

    debug dio

--- lib/Pages/bloc_providers.dart
+++ lib/Pages/bloc_providers.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ulearning_app/Pages/register/bloc/register_blocs.dart';
 import 'package:ulearning_app/Pages/sign_in/bloc/sign_in_blocs.dart';
 import 'package:ulearning_app/Pages/welcome/bloc/welcome_blocs.dart';
-import 'package:ulearning_app/app_blocs.dart';
+
 
 class AppBlocProviders{
   static get allBlocProviders=>[
