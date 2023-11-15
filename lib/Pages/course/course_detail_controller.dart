9ba9ba8965721c7a4354bf1eba0a0165e9931870
Aga Ilham <agailham86@gmail.com>
2023-11-15 16:09:12 +0700

    add course detail controller

---
+++ lib/Pages/course/course_detail_controller.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class CourseDetailController{
+  final BuildContext context;
+  CourseDetailController({
+    required this.context
+  });
+  void init() async{
+    
+  }
+}
